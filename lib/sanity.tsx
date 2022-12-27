b8c9c4070b604fddd5fd4bbf37c34007071ac857
Hamed Sedighi <hamedsedighi@gmail.com>
2022-12-27 23:27:43 +0100

    update sanity config

--- lib/sanity.tsx
+++ lib/sanity.tsx
@@ -2,9 +2,9 @@ import imageUrlBuilder from '@sanity/image-url'
 import { PortableText as PortableTextComponent } from '@portabletext/react'
 import { createClient } from 'next-sanity'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "fw8jg0nk"
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production"
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2022-11-16"
 
 export const sanityClient = createClient({
   projectId,
